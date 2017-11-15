8a6a7c7a5e4f3ee99433a748bf6725c97461c50d
Szymon Dzialowski <sd@phase-ii.com>
2017-11-15 11:20:29 +0000

    forever

--- react/server.sh
+++ react/server.sh
@@ -15,7 +15,6 @@ echo "$NOW before and after restart:" >> $FILE
 ps aux | head -n 1 >> $FILE
 ps aux | grep pingserver | grep -v grep | grep -v "sh -c" | grep -v yarn >> $FILE
 
-
 SERVER_HOST=0.0.0.0
 SERVER_PORT=82
 
@@ -30,7 +29,25 @@ if [ "$#" == 0 ] || [ "$#" -gt 1 ] ; then
 	echo "to start server"
 else
 
-    node index.server.js pingserver 1>> ${LOGFILE} 2>> ${LOGFILE} & disown
+    # standard way of running server, but if it's gonna crash, there is no way to restart it automatically
+    # node index.server.js pingserver 1>> ${LOGFILE} 2>> ${LOGFILE} & disown
+
+    # i tested nodemon too but it seems to just run/restart when file change,
+    # don't restart after process crush (that's different behaviour)
+    # so it's not good neither
+        # to test run to kill: kill -SIGTERM $(ps aux | grep "pingserver" | grep -v grep | grep -v nodemon | grep -v "sh " | head -1 | awk '{print $2}') &> /dev/null
+    # nodemon --delay 3 -V index.server.js pingserver 1>> ${LOGFILE} 2>> ${LOGFILE} & disown
+
+
+    # third solution is "forever" tool installed from npm globally
+        # to test run to kill:
+        # kill -SIGTERM $(ps aux | grep 'react/index.server.js' | grep -v grep | head -1 | awk '{print $2}') &> /dev/null
+    # before install forever globally ->   npm install -g forever
+    forever --minUptime 5000 -v -a -c node index.server.js pingserver 1>> ${LOGFILE} 2>> ${LOGFILE} & disown
+    # --minUptime      Minimum uptime (millis) for a script to not be considered "spinning" (5000 -> 5 sec)
+    # --minUptime not set. Defaulting to: 1000ms
+    # --spinSleepTime  Time to wait (millis) between launches of a spinning script.
+    # --spinSleepTime not set. Your script will exit if it does not stay up for at least 1000ms
 
     sleep 3
 
@@ -39,6 +56,8 @@ else
     echo 'testing servers...';
 
     curl "${SERVER_HOST}:${SERVER_PORT}" &> /dev/null && echo 'SERVER     : is working' || echo 'SERVER     : not working';
+
+    echo "check also log file of this process ${LOGFILE}";
 
 fi
 
